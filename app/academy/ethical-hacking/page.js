97918a08f5ab5f8498560cdda1855ae0b47b26ef
Divine Ezelibe <divineezelibe.e@gmail.com>
2026-07-01 14:42:48 +0100

    Updated and added the Ethical Hacking

--- app/academy/ethical-hacking/page.js
+++ app/academy/ethical-hacking/page.js
@@ -1,4 +1,11 @@
 import EthicalHacking from "@/modules/academy/ethical-hacking/index";
+import courseData from "@/modules/academy/ethical-hacking/data.json";
+import { buildCourseMetadata } from "@/modules/academy/shared/build-metadata";
+
+export const metadata = buildCourseMetadata(
+  courseData,
+  "https://www.digitanotion.com.ng/academy/ethical-hacking"
+);
 
 export default function EthicalHackingPage() {
   return <EthicalHacking />;
